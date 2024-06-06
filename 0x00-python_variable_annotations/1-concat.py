143df3f68cd53eea52c619d388e50546035f8605
Emran <ouakouriimran@gmail.com>
2024-06-06 15:14:39 +0100

    docs

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Implement a function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of the two strings.
+    """
+    return str1 + str2
