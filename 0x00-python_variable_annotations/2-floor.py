b9850b2aa1dc639282a041cd6b61638a2286dce9
Emran <ouakouriimran@gmail.com>
2024-06-06 15:16:49 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Implement a function that returns the floor of a float."""
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The float to round down.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return int(n)
